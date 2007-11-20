451075ee982771789c44a7cd58cbb9ca10036469
ivan_rome <ivan_rome>
2007-11-20 23:11:17 +0000

    *** empty log message ***

--- p2/Lapiz.cpp
+++ p2/Lapiz.cpp
@@ -23,15 +23,42 @@ void Lapiz::lineTo (Punto2f * destino, bool esVisible){
 
 
 void Lapiz::gira (GLfloat incrAng){
-ang += incrAng;
-GLfloat grados = r2g(ang);
+        ang += incrAng;
+        GLfloat grados = r2g(ang);
 
-double  p_E;
-double parte_Decimal = modf(grados, &p_E);
-int  parte_Entera = p_E;
-parte_Entera = parte_Entera % 360;
-grados = g2r(parte_Entera);
-grados += g2r(parte_Decimal);
-
+        double  p_E;
+        double parte_Decimal = modf(grados, &p_E);
+        int  parte_Entera = p_E;
+        parte_Entera = parte_Entera % 360;
+        grados = g2r(parte_Entera);
+        grados += g2r(parte_Decimal);
 }
 
+void Lapiz::avanza (GLfloat longitud, bool esVisible){
+GLfloat xD = pos -> getX() + longitud * cos (ang);
+GLfloat yD = pos -> getX() + longitud * sin (ang);
+Punto2f  * p = new Punto2f(xD,yD);
+lineTo(p,esVisible);
+delete p;
+}
+
+void Lapiz::drawMotivo(Lapiz *l){
+
+}
+void Lapiz::drawTotal (Punto2f * posInicial, GLfloat angInicial){
+
+}
+void Lapiz::poliEspiral ( Punto2f * posInicial, GLfloat incrAng,
+                   GLfloat angInicial, GLfloat incrLong,
+                   GLfloat longInicial, int nPasos){
+
+                   }
+void Lapiz::poligonoR1 (Lapiz * l, GLfloat lado, int nlados){
+
+}
+void Lapiz::poligonoR2 (Punto2f * centro, GLfloat radio, int nlados){
+
+}
+void Lapiz::arco(Punto2f * inicio, Punto2f * fin, Punto2f *otro){
+
+}
--- p2/Lapiz.h
+++ p2/Lapiz.h
@@ -20,5 +20,13 @@ public:
 Lapiz();
 void lineTo (Punto2f *, bool);
 void gira (GLfloat incrAng);
-
+void avanza (GLfloat longitud, bool esVisible);
+void drawMotivo(Lapiz *l);
+void drawTotal (Punto2f * posInicial, GLfloat angInicial);
+void poliEspiral ( Punto2f * posInicial, GLfloat incrAng,
+                   GLfloat angInicial, GLfloat incrLong,
+                   GLfloat longInicial, int nPasos);
+void poligonoR1 (Lapiz * l, GLfloat lado, int nlados);
+void poligonoR2 (Punto2f * centro, GLfloat radio, int nlados);
+void arco(Punto2f * inicio, Punto2f * fin, Punto2f *otro);
 };
