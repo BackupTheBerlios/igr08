3515c549a69d86ef7351cb6f6700bd1a5e8d74f5
ivan_rome <ivan_rome>
2007-11-21 12:23:55 +0000

    *** empty log message ***

--- p2/Segmento.cpp
+++ p2/Segmento.cpp
@@ -8,3 +8,18 @@
 //---------------------------------------------------------------------------
 
 #pragma package(smart_init)
+    Segmento::Segmento() {
+         inicio = new Punto2f (0.0, 0.0);
+         final  = new Punto2f (0.0, 0.0);
+      }
+
+      Segmento::Segmento(Punto2f i, Punto2f f) {
+         inicio = i.clon();
+         final = f.clon();
+      }
+
+
+   Segmento::~Segmento(){
+         delete inicio;
+         delete final;
+   }
--- p2/Segmento.h
+++ p2/Segmento.h
@@ -1,31 +1,30 @@
 //---------------------------------------------------------------------------
-#define"punto2f.h"
+
 
 #ifndef SegmentoH
 #define SegmentoH
+
+#include "Punto2f.h"
 //---------------------------------------------------------------------------
 
 class Segmento{
 
-private:
-Punto2f * inicio;
-Punto2f * final;
+   private:
+      Punto2f * inicio;
+      Punto2f * final;
 
-public:
-Segmento() {
-inicio = new Punto2f(0.0, 0.0);
-final = new Puntof(0.0, 0.0)
-}
+   public:
+      Segmento();
 
-Segmento(Punto2f i, Punto2f f) {}
-   inicio = i.clon();
-   final = f.clon();
-}
+      Segmento(Punto2f i, Punto2f f);
+
+      ~Segmento();
 
 
-~Segmento(){
-   delete inicio;
-   delete final;
-}
+      Punto2f * getInicio(){return inicio;}
+      Punto2f * getFinal(){return final;}
+      void setInicio(Punto2f p);
+      void setFinal(Punto2f p);
 
+};
 #endif
