cbcf0498a64ace7286ecff290fdc8e975ced4bbe
ivan_rome <ivan_rome>
2008-03-05 19:13:31 +0000

    no message

--- p4/src/practica4/Cara.java
+++ p4/src/practica4/Cara.java
@@ -3,10 +3,16 @@ package practica4;
 import java.util.ArrayList;
 
 public class Cara {
+
     private ArrayList vertices;
     private ArrayList normales;
-    
+
     public Cara() {
+	vertices = new ArrayList<PuntoVector3D>();
+	normales = new ArrayList<PuntoVector3D>();
     }
-    
+
+    public void SetNormales() {
+    // Metodo de Newel
+    }
 }
