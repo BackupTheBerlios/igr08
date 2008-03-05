a0a8abf5d82a75bfe82647fa882781f6755bc7be
ivan_rome <ivan_rome>
2008-03-05 20:00:06 +0000

    no message

--- p4/src/practica4/Cara.java
+++ p4/src/practica4/Cara.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Cara {
 
-    private ArrayList vertices;
-    private ArrayList normales;
+    private ArrayList<PuntoVector3D> vertices;
+    private ArrayList<PuntoVector3D> normales;
 
     public Cara() {
 	vertices = new ArrayList<PuntoVector3D>();
@@ -13,6 +13,20 @@ public class Cara {
     }
 
     public void SetNormales() {
-    // Metodo de Newel
+	// Metodo de Newel
+	double nx=0, ny=0, nz=0;
+	int N = vertices.size();
+	for (int i = 0; i < N; i++) {
+	    nx = vertices.get(i).getY()-(vertices.get(i + 1 % N).getY());
+	    nx = nx * vertices.get(i).getZ()+(vertices.get(i + 1 % N).getZ());
+	    
+	    ny = vertices.get(i).getZ()-(vertices.get(i + 1 % N).getZ());
+	    ny = ny * vertices.get(i).getX()+(vertices.get(i + 1 % N).getX());
+	    
+	    nz = vertices.get(i).getX()-(vertices.get(i + 1 % N).getX());
+	    nz = nz * vertices.get(i).getY()+(vertices.get(i + 1 % N).getY());
+	}
+	normales.add(new PuntoVector3D(nx, ny, nz));
+		
     }
 }
