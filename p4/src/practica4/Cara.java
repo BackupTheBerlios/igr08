d1a104eefbddb6bce153b5e48a50e7bb414829fc
ivan_rome <ivan_rome>
2008-03-05 22:26:45 +0000

    no message

--- p4/src/practica4/Cara.java
+++ p4/src/practica4/Cara.java
@@ -14,19 +14,31 @@ public class Cara {
 
     public void SetNormales() {
 	// Metodo de Newel
-	double nx=0, ny=0, nz=0;
+	double nx = 0, ny = 0, nz = 0;
 	int N = vertices.size();
 	for (int i = 0; i < N; i++) {
-	    nx = vertices.get(i).getY()-(vertices.get(i + 1 % N).getY());
-	    nx = nx * vertices.get(i).getZ()+(vertices.get(i + 1 % N).getZ());
-	    
-	    ny = vertices.get(i).getZ()-(vertices.get(i + 1 % N).getZ());
-	    ny = ny * vertices.get(i).getX()+(vertices.get(i + 1 % N).getX());
-	    
-	    nz = vertices.get(i).getX()-(vertices.get(i + 1 % N).getX());
-	    nz = nz * vertices.get(i).getY()+(vertices.get(i + 1 % N).getY());
+	    nx = vertices.get(i).getY() - (vertices.get(i + 1 % N).getY());
+	    nx = nx * vertices.get(i).getZ() + (vertices.get(i + 1 % N).getZ());
+
+	    ny = vertices.get(i).getZ() - (vertices.get(i + 1 % N).getZ());
+	    ny = ny * vertices.get(i).getX() + (vertices.get(i + 1 % N).getX());
+
+	    nz = vertices.get(i).getX() - (vertices.get(i + 1 % N).getX());
+	    nz = nz * vertices.get(i).getY() + (vertices.get(i + 1 % N).getY());
 	}
 	PuntoVector3D tmp = new PuntoVector3D(nx, ny, nz);
-	normales.add(tmp.normaliza());		
+	normales.add(tmp.normaliza());
+    }
+
+    public int getNumVertices() {
+	return vertices.size();
+    }
+    
+    public int getIndiceVertice(int i){
+	return -1; // terminar
+    }
+    
+    public int getIndiceNormal(int i){
+	return -1; // terminar
     }
 }
