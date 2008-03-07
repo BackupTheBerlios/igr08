3429dfb96f8e01632f6be51f97eec0d3952cd230
ivan_rome <ivan_rome>
2008-03-07 01:43:34 +0000

    no message

--- p4/src/practica4/Principal.java
+++ p4/src/practica4/Principal.java
@@ -8,45 +8,38 @@ import javax.media.opengl.*;
 import com.sun.opengl.util.*;
 
 public class Principal extends JFrame {
-    
+
     // Atributos
     private Container panel;
-    
     private GLJPanel canvas;
     private GL3D escena;
-       
+
     // Constructora
     public Principal() {
-        
-        // Titulo de la ventana y tamaño
+
+	// Titulo de la ventana y tamaño
 	setTitle("Práctica 4");
-        setSize(new Dimension(400,400));
-        super.setResizable(false);
-        
-        // Contenedor del editor
-        panel = this.getContentPane();
-        panel.setBackground(Color.black);
-        panel.setLayout(null);
-        
-        
-        // Creamos el canvas de dibujo
-        canvas = new GLJPanel();
-        escena = new GL3D();
-        canvas.addGLEventListener(escena);
-        canvas.setBounds(0,0,400,400);
-        panel.add(canvas);
-        
+	setSize(new Dimension(400, 400));
+	super.setResizable(false);
+
+	// Contenedor del editor
+	panel = this.getContentPane();
+	panel.setBackground(Color.black);
+	panel.setLayout(null);
+
+
+	// Creamos el canvas de dibujo
+	canvas = new GLJPanel();
+	escena = new GL3D();
+	canvas.addGLEventListener(escena);
+	canvas.setBounds(0, 0, 400, 400);
+	panel.add(canvas);
+
     }
-    
-    
-    
-    
-    
-    
-    
+
     // Métood Main
     public static void main(String[] args) {
-        // TODO code application logic here
+	Principal p4 = new Principal();
+	p4.setVisible(true);
     }
-    
 }
