5d154c7c80682899f25af608b0650cfc398d0097
ivan_rome <ivan_rome>
2008-03-29 18:16:58 +0000

    no message

--- p4/src/practica4/util/Lapiz.java
+++ p4/src/practica4/util/Lapiz.java
@@ -1,7 +1,7 @@
 package practica4.util;
 
+import java.util.ArrayList;
 import practica4.Modelo.PuntoVector3D;
-import javax.media.opengl.GL;
 
 public class Lapiz {
 
@@ -17,19 +17,11 @@ public class Lapiz {
 // Constructora por parámetros
     public Lapiz(PuntoVector3D p, float a) {
 	ang = a;
-    //pos = p -> clon();
-
+	pos = p;
     }
 
 // Dibuja un segmento desde la posicion relativa
-    void lineTo( PuntoVector3D destino, boolean esVisible) {
-
-	/*if (esVisible) {
-	    gl.glBegin(gl.GL_LINES);
-	    gl.glVertex2d(pos.getX(), pos.getY());
-	    gl.glVertex2d(destino.getX(), destino.getY());
-	    gl.glEnd();
-	}*/
+    void lineTo(PuntoVector3D destino) {
 	pos = destino.clonar();
     }
 
@@ -37,40 +29,39 @@ public class Lapiz {
     void gira(double incrAng) {
 
 	double sum = incrAng + Conversiones.r2g(ang);
-	double p_E;
 	double parte_Decimal = Conversiones.parteDecimal(sum);
 	int parte_Entera = (int) sum;
 	parte_Entera = parte_Entera % 360;
 	double grados = Conversiones.g2r(parte_Entera);
 	grados += Conversiones.g2r(parte_Decimal);
 	ang = grados;
-
-
     }
 
 // Avanzamos al siguiente punto relativo
-    public void avanza(double longitud, boolean esVisible, Segmento s) {
+    public void avanza(double longitud, Segmento s) {
 
 	double xD = pos.getX() + longitud * Math.cos(ang);
 	double yD = (float) pos.getY() + longitud * Math.sin(ang);
 	PuntoVector3D p = new PuntoVector3D((float) xD, (float) yD, 0);
 	s = new Segmento(pos, p);
-	lineTo(p, esVisible);
+	lineTo(p);
     }
 
-    void poligonoR1(double lado, int nlados) {
+    public ArrayList<PuntoVector3D> poligonoR1(double lado, int nlados) {
+	ArrayList<PuntoVector3D> retVal = new ArrayList<PuntoVector3D>();
 	double alfa = 360.0 / (double) nlados;
 	double beta = (180.0 - alfa) / 2.0;
 	double gamma = 180 - 2 * beta;
 	for (int i = 0; i < nlados; i++) {
 	    Segmento s = new Segmento();
-	    this.avanza(lado, true, s);
+	    this.avanza(lado, s);
 	    this.gira(gamma);
 	}
+	return retVal;
     }
 
-//---------------------------------------------------------------------------
-    void poligonoR2(PuntoVector3D centro, float radio, int nlados) {
+    public ArrayList<PuntoVector3D> poligonoR2(PuntoVector3D centro, float radio, int nlados) {
+	ArrayList<PuntoVector3D> retVal = new ArrayList<PuntoVector3D>();
 	double alfa = 360.0 / (double) nlados;
 	double beta = (180.0 - alfa) / 2.0;
 	//   GLdouble gamma = 180 - 2*beta;
@@ -82,7 +73,6 @@ public class Lapiz {
 
 	double lado = Math.cos(Conversiones.g2r(beta)) * radio * 2;
 	poligonoR1(lado, nlados);
+	return retVal;
     }
-
-//---------------------------------------------------------------------------
 }
