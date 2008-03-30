3b03d64a64751c56d9771fa830a2bfb4d446fbdb
ivan_rome <ivan_rome>
2008-03-30 22:20:22 +0000

    no message

--- p4/src/practica4/Vista/Principal.java
+++ p4/src/practica4/Vista/Principal.java
@@ -220,8 +220,8 @@ public class Principal extends JFrame {
 
 	    public void actionPerformed(ActionEvent e) {
 
-		mallaExtrusion = new Toro(50.0f, 10.0f, 20, 10);
-		
+		//mallaExtrusion = new Toro(50.0f, 10.0f, 20, 10);
+		mallaExtrusion = new Toro(10, 50.0f, 10);
 
 		escena.actualizarMallaExt(1, mallaExtrusion);
 		escena.setGenerado(true);
