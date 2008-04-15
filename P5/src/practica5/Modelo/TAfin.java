ca453ad4ee9e89962a6876bd5fbcec9783868e69
turokdinosaur <turokdinosaur>
2008-04-15 20:33:10 +0000

    no message

--- P5/src/practica5/Modelo/TAfin.java
+++ P5/src/practica5/Modelo/TAfin.java
@@ -4,26 +4,30 @@ import javax.media.opengl.GL;
 
 public class TAfin {
     
-    private  double[] matriz;
+    // Atributos privados
     private GL gl;
+    private  double[] matriz;
     
+    // Constructora
     public TAfin() {
+        // Trabajamos con el formato de matriz equivalente a openGL 
         matriz = new double[16];
     }
     
-    public void gira(double ang, double ejeX, double ejeY, double ejeZ){
+    // Transformaciones Afines
+    public void rotar(double ang, double ejeX, double ejeY, double ejeZ){
         gl.glLoadIdentity();  //??
         gl.glRotated(ang, ejeX, ejeY, ejeZ);
         gl.glGetDoublev(gl.GL_MODELVIEW_MATRIX, matriz, 0);
     }
     
-    public void translada(double X, double Y, double Z){
+    public void trasladar(double X, double Y, double Z){
         gl.glLoadIdentity();  //??
         gl.glTranslated(X, Y, Z);
         gl.glGetDoublev(gl.GL_MODELVIEW_MATRIX, matriz, 0);
     }
     
-    public void escala(double X, double Y, double Z){
+    public void escalar(double X, double Y, double Z){
         gl.glLoadIdentity();  //??
         gl.glScaled(X, Y, Z);
         gl.glGetDoublev(gl.GL_MODELVIEW_MATRIX, matriz, 0);
