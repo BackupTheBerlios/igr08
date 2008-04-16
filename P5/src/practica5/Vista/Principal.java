f0047c72f5245800cb541f86886b8aeba2add8e6
turokdinosaur <turokdinosaur>
2008-04-16 08:36:21 +0000

    no message

--- P5/src/practica5/Vista/Principal.java
+++ P5/src/practica5/Vista/Principal.java
@@ -26,12 +26,10 @@ public class Principal extends JFrame {
     private JMenuItem salir;
     private JMenu sobre;
     private JMenuItem autores;
-    private JButton botonDefinirPuntos;
-    private JButton botonGenerarMallaRevolucion;
-    private JButton botonGenerarMallaExtrusion;
-    private JButton botonAplicarSplines;
+
     private JButton botonCambiarModo;
     private JButton botonDibujarNormales;
+    
     private int tipoMalla;
     private boolean entradaDatos;
     private final Animator animacion;
@@ -44,7 +42,7 @@ public class Principal extends JFrame {
         
         // Titulo de la ventana y tama駉
         setTitle("Pr醕tica 5");
-        setSize(new Dimension(425, 500));
+        setSize(new Dimension(800, 600));
         super.setResizable(false);
         
         // Contenedor del editor
@@ -71,25 +69,9 @@ public class Principal extends JFrame {
         
         // Creamos los botones
         this.panelBotones = new JPanel();
-        panelBotones.setLayout(new GridLayout(3, 3));
+        panelBotones.setLayout(new GridLayout(1, 2));
         panel.add(panelBotones, BorderLayout.SOUTH);
-        
-        botonDefinirPuntos = new JButton("Definir Perfil");
-        botonDefinirPuntos.setVisible(true);
-        panelBotones.add(botonDefinirPuntos);
-        
-        botonAplicarSplines = new JButton("BSplines");
-        botonAplicarSplines.setVisible(true);
-        panelBotones.add(botonAplicarSplines);
-        
-        botonGenerarMallaRevolucion = new JButton("Revoluci髇");
-        botonGenerarMallaRevolucion.setVisible(true);
-        panelBotones.add(botonGenerarMallaRevolucion);
-        
-        botonGenerarMallaExtrusion = new JButton("Extrusi髇");
-        botonGenerarMallaExtrusion.setVisible(true);
-        panelBotones.add(botonGenerarMallaExtrusion);
-        
+                       
         botonCambiarModo = new JButton("Cambiar Modo");
         botonCambiarModo.setVisible(true);
         panelBotones.add(botonCambiarModo);
@@ -150,90 +132,7 @@ public class Principal extends JFrame {
                         JOptionPane.INFORMATION_MESSAGE);
             }
         });
-        
-        // Evento Oyente para el boton "Definir Perfil"
-        botonDefinirPuntos.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                perfil = new ArrayList<PuntoVector3D>();
-                escena.setPerfil(perfil);
-                entradaDatos = true;
-            }
-        });
-        
-        // Evento Oyente para el boton "Aplicar Splines"
-        botonAplicarSplines.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                
-                String dato = JOptionPane.showInputDialog(null, "Numero de puntos de control. Rango[" + perfil.size() + "...N]",
-                        "Datos de Entrada", 1);
-                if (dato != null) {
-                    int num = Integer.parseInt(dato);
-                    perfil = new Calculos().calculaPuntosBSplines(perfil, num);
-                    if (perfil.size() > num) {
-                        ArrayList<PuntoVector3D> perfilAux = new ArrayList<PuntoVector3D>();
-                        for (int i = 0; i < num - 2; i++) {
-                            perfilAux.add(perfil.get(i));
-                        }
-                        perfil = (ArrayList<PuntoVector3D>) perfilAux.clone();
-                    }
-                    escena.setPerfil(perfil);
-                    entradaDatos = false;
-                }
-            }
-        });
-        
-        // Evento Oyente para el boton "Malla Revoluci贸n"
-        botonGenerarMallaRevolucion.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                
-                //String datoNumVertices = "4";
-                String datoAng = JOptionPane.showInputDialog("羘gulo de rotaci髇: ", "0.15");
-                if (datoAng != null) {
-                    
-                    Object[] opciones = {"3", "4"};
-                    int eleccion = JOptionPane.showOptionDialog(null, "N鷐ero de V閞tices de la cara: ", "Datos de Entrada",
-                            JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, opciones, opciones[0]);
-                    
-                    int numVerticesCara = Integer.parseInt((String) opciones[eleccion]);
-                    double anguloRadianes = Double.parseDouble(datoAng);
-                    
-                    mallaActual = new MallaPorRevolucion(perfil, numVerticesCara, anguloRadianes);
-                    //  mallaRevolucion = new MallaPorRevolucion(perfil, numVerticesCara, anguloRadianes);
-                    
-                    //  escena.actualizarMallaRev(2, mallaRevolucion);
-                    escena.actualizarMalla(2, mallaActual);
-                    escena.setGenerado(true);
-                    entradaDatos = false;
-                }
-            }
-        });
-        
-        // Evento Oyente para el boton "Malla Extrusi贸n"
-        botonGenerarMallaExtrusion.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                
-                String SnP = JOptionPane.showInputDialog("N鷐ero de lados que aproximan la secci髇 del toroide", "10");
-                String SnQ = JOptionPane.showInputDialog("N鷐ero de capas del toroide", "50");
-                String Sr1 = JOptionPane.showInputDialog("Radio del toro", "150");
-                String Sr2 = JOptionPane.showInputDialog("Radio de la secci贸n del toro", "20");
-                
-                int nP = Integer.parseInt(SnP);
-                int nQ = Integer.parseInt(SnQ);
-                float r1 = Float.parseFloat(Sr1);
-                float r2 = Float.parseFloat(Sr2);
-                
-                mallaActual = new Toro(nP, nQ, r1, r2);
-                escena.actualizarMalla(3, mallaActual);
-                
-                escena.setGenerado(true);
-                entradaDatos = false;
-            }
-        });
-        
+
         // Evento Oyente para el bot贸n "Cambiar Modo"
         botonCambiarModo.addActionListener(new ActionListener() {
             
