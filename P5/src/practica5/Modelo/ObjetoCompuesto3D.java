03156fa0efbef578a68692ef7d83a9f0bd13f033
turokdinosaur <turokdinosaur>
2008-04-16 08:58:03 +0000

    no message

--- P5/src/practica5/Modelo/ObjetoCompuesto3D.java
+++ P5/src/practica5/Modelo/ObjetoCompuesto3D.java
@@ -8,8 +8,30 @@ public class ObjetoCompuesto3D extends Objeto3D{
     protected ArrayList<Objeto3D> hijos;
     
     // Constructora
-    public ObjetoCompuesto3D() {}
+    public ObjetoCompuesto3D() {
+        hijos = new ArrayList<Objeto3D>();
+    }
     
     // Operaciones 
+    void rotar(double ang, double ejeX, double ejeY, double ejeZ) {
+
+    }
+    
+    void trasladar(double X, double Y, double Z) {
+    
+    }
+    
+    void escalar(double X, double Y, double Z) {
+    
+    }
+    
+    // Getters & Setters
+    public ArrayList<Objeto3D> getHijos() {
+        return hijos;
+    }
+    
+    public void setHijos(Objeto3D objeto3d) {
+        hijos.add(objeto3d);
+    }
     
 }
