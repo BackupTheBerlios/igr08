b5b735fd46f3774cabd3b70d4df9823f228db555
turokdinosaur <turokdinosaur>
2008-04-16 08:46:14 +0000

    no message

--- P5/src/practica5/Modelo/Objeto3D.java
+++ P5/src/practica5/Modelo/Objeto3D.java
@@ -1,10 +1,50 @@
 package practica5.Modelo;
 
 public class Objeto3D {
+    
+    // Atributos protegidos
+    private int id;
     private Color color;
     private TAfin matriz;
+    private PuntoVector3D pos;
     
-    public Objeto3D() {
+    // Constructora
+    public Objeto3D() {}
+    
+    // Método virtual
+    public void dibuja() {};
+    
+    // Getters & Setters
+    public PuntoVector3D getPos() {
+        return pos;
     }
-    
+
+    public void setPos(PuntoVector3D pos) {
+        this.pos = pos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public TAfin getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(TAfin matriz) {
+        this.matriz = matriz;
+    }
+
 }
