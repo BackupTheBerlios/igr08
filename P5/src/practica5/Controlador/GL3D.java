a5146dec1868f63469b433f04197e67c6faf31ad
ivan_rome <ivan_rome>
2008-04-17 12:05:57 +0000

    no message

--- P5/src/practica5/Controlador/GL3D.java
+++ P5/src/practica5/Controlador/GL3D.java
@@ -8,238 +8,274 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
 import practica5.Modelo.Basic.Camara;
 import practica5.Modelo.Basic.Malla;
 import practica5.Modelo.Objetos.Toro;
 import practica5.Modelo.Basic.PuntoVector3D;
 
 public class GL3D implements GLEventListener {
-    
+
     // Variables Constantes
     public static int ORTOGONAL = 0;
     public static int PROYECCION = 1;
-    
     // Atributos privado
     private GLU glu;
     private GLContext context;
-    
-    private double xLeft, xRight;
-    private double yTop, yBot;
-    private double xCentro, yCentro;
-    
+    private double xLeft,  xRight;
+    private double yTop,  yBot;
+    private double xCentro,  yCentro;
     private int anchura;
     private int altura;
-    
     private Camara camara;
     private double RatioViewPort;
     private boolean generado;
-    
-    private static float[] POS_LUZ_0 = {25.0f, 25.0f, 0.0f, 1.0f};
-    
+    private int tipo;
+    private Malla mallaActual;
+    double eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ; // cï¿½mara
+    float[] PosicionLuz0 = new float[4];
+
     public GL3D(int anchura, int altura) {
-        
-        this.glu = new GLU();
-        
-        this.anchura = anchura;
-        this.altura = altura;
-        
-        this.xRight = anchura / 2.0;
-        this.xLeft = -xRight;
-        this.yTop = altura / 2.0;
-        this.yBot = -xRight;
-        this.xCentro = (xRight + xLeft) / 2.0;
-        this.yCentro = (yTop + yBot) / 2.0;
-        
-        this.RatioViewPort = 1.0;
-        this.generado = false;
-        
+
+	this.glu = new GLU();
+
+	this.anchura = anchura;
+	this.altura = altura;
+
+	this.xRight = anchura / 2.0;
+	this.xLeft = -xRight;
+	this.yTop = altura / 2.0;
+	this.yBot = -xRight;
+	this.xCentro = (xRight + xLeft) / 2.0;
+	this.yCentro = (yTop + yBot) / 2.0;
+
+	this.RatioViewPort = 1.0;
+	this.generado = false;
+
     }
-    
+
     public void display(GLAutoDrawable drw) {
-        GL gl = drw.getGL();
-        
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_COLOR_BUFFER_BIT);
-        
-        gl.glColor3d(100.0, 100.0, 250.0);
-        pruebas(gl);
-        
-        if (generado) {
-            //this.mallaActual.dibuja(gl);
-        }
-        
-        gl.glFlush();
+	GL gl = drw.getGL();
+	try{
+	Thread.sleep(100);
+	} catch (Exception e){}
+	gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_COLOR_BUFFER_BIT);
+
+	gl.glColor3d(100.0, 100.0, 250.0);
+	pruebas(gl);
+
+	if (generado) {
+	//this.mallaActual.dibuja(gl);
+	}
+
+	gl.glFlush();
     }
-    
-    public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {}
-    
+
+    public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
+    }
+
     public void init(GLAutoDrawable drw) {
-        GL gl = drw.getGL();
-        GLU glu = new GLU();
-        
-        //gl.glClearColor(0.6f, 0.7f, 0.8f, 1.0f);
-        
-        this.camara = new Camara(gl);
-        this.activarLuces(gl);
-        this.activarOpcionesOpenGL(gl);
-        
-        gl.glMatrixMode(gl.GL_MODELVIEW);
-        gl.glLoadIdentity();
-        glu.gluLookAt(camara.getEye().getX(), camara.getEye().getY(), camara.getEye().getZ(),
-                      camara.getLook().getX(), camara.getLook().getY(), camara.getLook().getZ(),
-                      camara.getUp().getX(), camara.getUp().getY(), camara.getUp().getZ());
-        
-        
-        gl.glClearColor(0, 0, 0, 0);
-        
-        gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
-        
-        //glu.gluOrtho2D(xLeft, xRight, yBot, yTop);
-        //glu.gluPerspective(45.0, xRight, 1.0, 20.0);
-        gl.glOrtho(xLeft, xRight, yBot, yTop, -100.0f, 100.0f);
-        
-        
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
-        
+	GL gl = drw.getGL();
+	GLU glu = new GLU();
+
+	//gl.glClearColor(0.6f, 0.7f, 0.8f, 1.0f);
+
+	this.camara = new Camara(gl);
+	this.activarLuces(gl);
+	this.activarOpcionesOpenGL(gl);
+
+
+	float LuzAmbiente[] = {0.5f, 0.5f, 0.5f, 1.0f};
+	FloatBuffer LuzAmbiente1 = FloatBuffer.wrap(LuzAmbiente);
+	gl.glLightfv(gl.GL_LIGHT0, gl.GL_AMBIENT, LuzAmbiente1);
+	PosicionLuz0[0] = 25.0f;
+	PosicionLuz0[1] = 25.0f;
+	PosicionLuz0[2] = 0.0f;
+	PosicionLuz0[3] = 1.0f;
+
+	FloatBuffer PosicionLuz01 = FloatBuffer.wrap(PosicionLuz0);
+	gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, PosicionLuz01);
+
+	gl.glEnable(gl.GL_COLOR_MATERIAL);
+	gl.glMaterialf(gl.GL_FRONT, gl.GL_SHININESS, 0.1f);
+	//gl.glEnable(gl.GL_DEPTH_TEST);
+	gl.glEnable(gl.GL_NORMALIZE);
+	//gl.glFrontFace(gl.GL_CCW);
+	//gl.glFrontFace(gl.GL_CW);
+	//gl.glEnable(gl.GL_FRONT_FACE);
+	//gl.glEnable(gl.GL_CULL_FACE);
+	gl.glShadeModel(gl.GL_SMOOTH);   //defecto
+
+	// cÃ¡mara
+	eyeX = 2.0;
+	eyeY = 2.0;
+	eyeZ = 2.0;
+	lookX = 0.0;
+	lookY = 0.0;
+	lookZ = 0.0;
+	upX = 0;
+	upY = 1;
+	upZ = 0;
+	gl.glMatrixMode(gl.GL_MODELVIEW);
+	gl.glLoadIdentity();
+	glu.gluLookAt(camara.getEye().getX(), camara.getEye().getY(), camara.getEye().getZ(),
+		camara.getLook().getX(), camara.getLook().getY(), camara.getLook().getZ(),
+		camara.getUp().getX(), camara.getUp().getY(), camara.getUp().getZ());
+
+
+	gl.glClearColor(0, 0, 0, 0);
+
+	gl.glMatrixMode(GL.GL_PROJECTION);
+	gl.glLoadIdentity();
+
+	//glu.gluOrtho2D(xLeft, xRight, yBot, yTop);
+	//glu.gluPerspective(45.0, xRight, 1.0, 20.0);
+	gl.glOrtho(xLeft, xRight, yBot, yTop, -100.0f, 100.0f);
+
+
+	gl.glMatrixMode(GL.GL_MODELVIEW);
+	gl.glLoadIdentity();
+
     }
-    
+
     public void reshape(GLAutoDrawable drw, int x, int y, int width, int height) {
-        RatioViewPort = (double) width / (double) height;
-        double RatioVolVista = (xRight - xLeft) / (yTop - yBot);
-        
-        if (RatioVolVista >= RatioViewPort) {
-            //Aumentamos yTop-yBot
-            double altoNew = (xRight - xLeft) / RatioViewPort;
-            yTop = yCentro + altoNew / 2.0;
-            yBot = yCentro - altoNew / 2.0;
-        } else {
-            //Aumentamos xRight-xLeft
-            double anchoNew = RatioViewPort * (yTop - yBot);
-            xRight = xCentro + anchoNew / 2.0;
-            xLeft = xCentro - anchoNew / 2.0;
-        }
-        
-        GL gl = drw.getGL();
-        gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
-        gl.glOrtho(xLeft, xRight, yBot, yTop, -1000.0f, 1000.0f);
-        
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
-        display(drw);
+	RatioViewPort = (double) width / (double) height;
+	double RatioVolVista = (xRight - xLeft) / (yTop - yBot);
+
+	if (RatioVolVista >= RatioViewPort) {
+	    //Aumentamos yTop-yBot
+	    double altoNew = (xRight - xLeft) / RatioViewPort;
+	    yTop = yCentro + altoNew / 2.0;
+	    yBot = yCentro - altoNew / 2.0;
+	} else {
+	    //Aumentamos xRight-xLeft
+	    double anchoNew = RatioViewPort * (yTop - yBot);
+	    xRight = xCentro + anchoNew / 2.0;
+	    xLeft = xCentro - anchoNew / 2.0;
+	}
+
+	GL gl = drw.getGL();
+	gl.glMatrixMode(GL.GL_PROJECTION);
+	gl.glLoadIdentity();
+	gl.glOrtho(xLeft, xRight, yBot, yTop, -1000.0f, 1000.0f);
+
+	gl.glMatrixMode(GL.GL_MODELVIEW);
+	gl.glLoadIdentity();
+	display(drw);
     }
-    
+
     public GLContext getContext() {
-        return context;
+	return context;
     }
-        
-    // M�todo que dibuja los puntos de control del perfil
+
+    // Mï¿½todo que dibuja los puntos de control del perfil
     public void dibujarPuntos(GL gl) {
-        
-        gl.glColor3f(1.0f, 1.0f, 1.0f);
-        gl.glPointSize(2.0f);
-        
-        gl.glBegin(GL.GL_POINTS);
-        
-        gl.glEnd();
+
+	gl.glColor3f(1.0f, 1.0f, 1.0f);
+	gl.glPointSize(2.0f);
+
+	gl.glBegin(GL.GL_POINTS);
+
+	gl.glEnd();
     }
-    
+
     // Activamos las luces del entorno
     public void activarLuces(GL gl) {
-        
-        // Activamos Luz en OpenGL
-        gl.glEnable(gl.GL_LIGHTING);
-        gl.glEnable(gl.GL_LIGHT0);
-        
-        // Luz 0
-        gl.glEnable(gl.GL_LIGHT0);
-        float LuzDifusa[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        FloatBuffer LuzDifusa1 = FloatBuffer.wrap(LuzDifusa);
-        gl.glLightfv(gl.GL_LIGHT0, gl.GL_DIFFUSE, LuzDifusa1);
-        
-        // Luz Ambiental
-        float LuzAmbiente[] = {0.5f, 0.5f, 0.5f, 1.0f};
-        FloatBuffer LuzAmbiente1 = FloatBuffer.wrap(LuzAmbiente);
-        gl.glLightfv(gl.GL_LIGHT0, gl.GL_AMBIENT, LuzAmbiente1);
-        FloatBuffer PosicionLuz01 = FloatBuffer.wrap(POS_LUZ_0);
-        gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, PosicionLuz01);
+
+	// Activamos Luz en OpenGL
+	gl.glEnable(gl.GL_LIGHTING);
+	gl.glEnable(gl.GL_LIGHT0);
+
+	// Luz 0
+	gl.glEnable(gl.GL_LIGHT0);
+	float LuzDifusa[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	FloatBuffer LuzDifusa1 = FloatBuffer.wrap(LuzDifusa);
+	gl.glLightfv(gl.GL_LIGHT0, gl.GL_DIFFUSE, LuzDifusa1);
+
+	// Luz Ambiental
+	float LuzAmbiente[] = {0.5f, 0.5f, 0.5f, 1.0f};
+	FloatBuffer LuzAmbiente1 = FloatBuffer.wrap(LuzAmbiente);
+	gl.glLightfv(gl.GL_LIGHT0, gl.GL_AMBIENT, LuzAmbiente1);
+	FloatBuffer PosicionLuz01 = FloatBuffer.wrap(PosicionLuz0);
+	gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, PosicionLuz01);
     }
-    
+
     // Activamos opciones internas de OpenGL
     public void activarOpcionesOpenGL(GL gl) {
-       
-        gl.glEnable(gl.GL_COLOR_MATERIAL);
-        gl.glMaterialf(gl.GL_FRONT, gl.GL_SHININESS, 0.1f);
-        //gl.glEnable(gl.GL_DEPTH_TEST);
-        gl.glEnable(gl.GL_NORMALIZE);
-        //gl.glFrontFace(gl.GL_CCW);
-        //gl.glFrontFace(gl.GL_CW);
-        //gl.glEnable(gl.GL_FRONT_FACE);
-        //gl.glEnable(gl.GL_CULL_FACE);
-        gl.glShadeModel(gl.GL_SMOOTH);   //defecto
+
+	gl.glEnable(gl.GL_COLOR_MATERIAL);
+	gl.glMaterialf(gl.GL_FRONT, gl.GL_SHININESS, 0.1f);
+	//gl.glEnable(gl.GL_DEPTH_TEST);
+	gl.glEnable(gl.GL_NORMALIZE);
+	//gl.glFrontFace(gl.GL_CCW);
+	//gl.glFrontFace(gl.GL_CW);
+	//gl.glEnable(gl.GL_FRONT_FACE);
+	//gl.glEnable(gl.GL_CULL_FACE);
+	gl.glShadeModel(gl.GL_SMOOTH);   //defecto
     }
-    
+
     // Getters & Setters
     public boolean getGenerado() {
-        return generado;
+	return generado;
     }
-    
+
     public void setGenerado(boolean bool) {
-        generado = bool;
+	generado = bool;
     }
-    
-    public Camara getCamara() {
-        return this.camara;
+
+    // MÃ©todos que actualiza datos de la escena
+    public void actualizarPerfil(int tipoMalla, ArrayList<PuntoVector3D> puntosPerfil) {
+	this.tipo = tipoMalla;
+	//this.perfil = (ArrayList<PuntoVector3D>) puntosPerfil.clone();
     }
-    
+
     public void setCamara(Camara c) {
-        this.camara = c;
+	this.camara = c;
     }
-    
-    
+
+    public Camara getCamara() {
+	return this.camara;
+    }
+
     // Escala un punto desde el puerto de vista hasta el area visible de la escena
     public PuntoVector3D convertirPuntoToPixel(PuntoVector3D punto) {
-        
-        PuntoVector3D pixel = new PuntoVector3D();
-        
-        double escalaAncho = (double) (anchura / (xRight - xLeft));
-        if (xLeft < 0) {
-            pixel.setX((double) (punto.getX() / escalaAncho + xLeft));
-        } else {
-            pixel.setX((double) (punto.getX() / escalaAncho - xLeft));
-        }
-        
-        double escalaAlto = (double) (altura / (yTop - yBot));
-        if (yTop < 0) {
-            pixel.setY((double) (punto.getY() / escalaAlto + yTop));
-        } else {
-            pixel.setY((float) (punto.getY() / escalaAlto - yTop));
-        }
-        
-        pixel.setZ(punto.getZ());
-        pixel.setPV(punto.getPV());
-        
-        return pixel;
+
+	PuntoVector3D pixel = new PuntoVector3D();
+
+	double escalaAncho = (double) (anchura / (xRight - xLeft));
+	if (xLeft < 0) {
+	    pixel.setX((double) (punto.getX() / escalaAncho + xLeft));
+	} else {
+	    pixel.setX((double) (punto.getX() / escalaAncho - xLeft));
+	}
+
+	double escalaAlto = (double) (altura / (yTop - yBot));
+	if (yTop < 0) {
+	    pixel.setY((double) (punto.getY() / escalaAlto + yTop));
+	} else {
+	    pixel.setY((float) (punto.getY() / escalaAlto - yTop));
+	}
+
+	pixel.setZ(punto.getZ());
+	pixel.setPV(punto.getPV());
+
+	return pixel;
     }
-    
-    // M�todo que convierte las coordenadas del perfil a coordenadas del Area Visible de la Escena
+
+    // MÃ©todo que convierte las coordenadas del perfil a coordenadas del Area Visible de la Escena
     public ArrayList<PuntoVector3D> transformarPerfil(ArrayList<PuntoVector3D> perfilDado) {
-        
-        ArrayList<PuntoVector3D> retVal = new ArrayList<PuntoVector3D>();
-        
-        for (int i = 0; i < perfilDado.size(); i++) {
-            retVal.add(convertirPuntoToPixel(perfilDado.get(i)));
-        }
-        
-        return retVal;
+
+	ArrayList<PuntoVector3D> retVal = new ArrayList<PuntoVector3D>();
+
+	for (int i = 0; i < perfilDado.size(); i++) {
+	    retVal.add(convertirPuntoToPixel(perfilDado.get(i)));
+	}
+
+	return retVal;
     }
-    
+
     // PRUEBA
     public void pruebas(GL gl) {
-        Malla mallaToro = new Toro(25,36,180.5f,90.0f);
-        mallaToro.dibuja(gl);
+	Malla mallaToro = new Toro(25, 36, 180.5f, 90.0f);
+	mallaToro.dibuja(gl);
     }
-    
 }
--- P5/src/practica5/Modelo/Basic/Camara.java
+++ P5/src/practica5/Modelo/Basic/Camara.java
@@ -5,166 +5,162 @@ import javax.media.opengl.glu.GLU;
 import practica5.util.Conversiones;
 
 public class Camara {
-    
+
     // Atributos privados
-    private PuntoVector3D eye, look, up;
-    private PuntoVector3D u, v, n;
-    private float l, r, b, t, N, F;
-    private double[] m= new double[16];
-    
+    private PuntoVector3D eye,  look,  up;
+    private PuntoVector3D u,  v,  n;
+    private float l,  r,  b,  t,  N,  F;
+    private double[] m = new double[16];
     private GL gl;
     private GLU glu;
-    
-    
+
     public Camara(GL gl) {
-        this.gl = gl;
-        this.glu = new GLU();
-        
-        eye = new PuntoVector3D(0,0,0);
-        look = new PuntoVector3D(0,0,-1);
-        up = new PuntoVector3D(0,1,0);
+	this.gl = gl;
+	this.glu = new GLU();
+
+	eye = new PuntoVector3D(0, 0, 0);
+	look = new PuntoVector3D(0, 0, -1);
+	up = new PuntoVector3D(0, 1, 0);
+
+	setView(eye, look, up);
+	setModelViewMatrix();
+	setProjection();
     }
-    
-    public Camara(GL gl, PuntoVector3D eye, PuntoVector3D look, PuntoVector3D up) {
-        this.gl = gl;
-        this.glu = new GLU();
-        
-        this.eye = eye;
-        this.look = look;
-        this.up = up;
+
+    private void setView(PuntoVector3D eye, PuntoVector3D look, PuntoVector3D up) {
+	this.eye = eye;
+	this.look = look;
+	this.up = up;
+	n = eye.menos(look);
+	n = n.normaliza();
+
+	u = up.cross(n);
+	u = u.normaliza();
+
+	v = n.cross(u);
+
+	gl.glMatrixMode(gl.GL_MODELVIEW);
+	gl.glLoadIdentity();
+
+	glu.gluLookAt(eye.getX(), eye.getY(), eye.getZ(), look.getX(), look.getY(), look.getZ(), up.getX(), up.getY(), up.getZ());
     }
-    
-    private void setView(PuntoVector3D eye, PuntoVector3D look, PuntoVector3D up){
-        this.eye = eye;
-        this.look = look;
-        this.up = up;
-        n = eye.menos(look);
-        n = n.normaliza();
-        
-        u = up.cross(n);
-        u = u.normaliza();
-        
-        v = n.cross(u);
-        
-        gl.glMatrixMode(gl.GL_MODELVIEW);
-        gl.glLoadIdentity();
-        
-        glu.gluLookAt(eye.getX(),eye.getY(),eye.getZ(),look.getX(),look.getY(),look.getZ(),up.getX(),up.getY(),up.getZ());
+
+    private void setProjection() {
+	gl.glMatrixMode(gl.GL_PROJECTION);
+	gl.glLoadIdentity();
+	//gl.glOrtho(l,r,b,t,-1,1);
+	double anguloVision = 10;
+	double proporcion = 1;
+	glu.gluPerspective(anguloVision, proporcion, N, F);
     }
-    
-    private void setProjection(){
-        gl.glMatrixMode(gl.GL_PROJECTION);
-        gl.glLoadIdentity();
-        //gl.glOrtho(l,r,b,t,-1,1);
-        double anguloVision = 10;
-        double proporcion = 1;
-        glu.gluPerspective(anguloVision, proporcion, N, F);
+
+    private void setModelViewMatrix() {
+	double x, y, z;
+	x = -eye.prodEsc(u);
+	y = -eye.prodEsc(v);
+	z = -eye.prodEsc(n);
+	PuntoVector3D d = new PuntoVector3D(x, y, z);
+	// 1 Fila
+	m[0] = u.x;
+	m[1] = u.y;
+	m[2] = u.z;
+	m[3] = d.x;
+	// 2 Fila
+	m[4] = v.x;
+	m[5] = v.y;
+	m[6] = v.z;
+	m[7] = d.y;
+	// 3 Fila
+	m[8] = n.x;
+	m[9] = n.y;
+	m[10] = n.z;
+	m[11] = d.z;
+	// 4 Fila
+	m[12] = 0;
+	m[13] = 0;
+	m[14] = 0;
+	m[15] = 1;
+
+	gl.glMatrixMode(gl.GL_MODELVIEW);
+	gl.glLoadMatrixd(m, 0);
+
     }
-    
-    private void setModelViewMatrix(){
-        double x , y , z;
-        x = -eye.prodEsc(u);
-        y = -eye.prodEsc(v);
-        z = -eye.prodEsc(n);
-        PuntoVector3D d = new PuntoVector3D(x, y, z);
-        // 1ª Fila
-        m[0] = u.x;
-        m[1] = u.y;
-        m[2] = u.z;
-        m[3] = d.x;
-        // 2ª Fila
-        m[4] = v.x;
-        m[5] = v.y;
-        m[6] = v.z;
-        m[7] = d.y;
-        // 3ª Fila
-        m[8] = n.x;
-        m[9] = n.y;
-        m[10] = n.z;
-        m[11] = d.z;
-        // 4ª Fila
-        m[12] = 0;
-        m[13] = 0;
-        m[14] = 0;
-        m[15] = 1;
+
+    public void roll(double angulo) {
+	double cs = Math.cos(Conversiones.g2r(angulo));
+	double sn = Math.sin(Conversiones.g2r(angulo));
+	PuntoVector3D t = u.clonar();
+	u = new PuntoVector3D(
+		cs * t.getX() - sn * v.getX(),
+		cs * t.getY() - sn * v.getY(),
+		cs * t.getZ() - sn * v.getZ());
+	v = new PuntoVector3D(
+		sn * t.getX() + cs * v.getX(),
+		sn * t.getY() + cs * v.getY(),
+		sn * t.getZ() + cs * v.getZ());
+	setModelViewMatrix();
     }
-    
-    public void roll(double angulo){
-        double cs = Math.cos(Conversiones.g2r(angulo));
-        double sn = Math.sin(Conversiones.g2r(angulo));
-        PuntoVector3D t = u.clonar();
-        u = new PuntoVector3D(
-                cs*t.getX()-sn*v.getX(),
-                cs*t.getY()-sn*v.getY(),
-                cs*t.getZ()-sn*v.getZ());
-        v = new PuntoVector3D(
-                sn*t.getX()+cs*v.getX(),
-                sn*t.getY()+cs*v.getY(),
-                sn*t.getZ()+cs*v.getZ());
-        setModelViewMatrix();
+
+    public void pitch(double angulo) {
+	double cs = Math.cos(Conversiones.g2r(angulo));
+	double sn = Math.sin(Conversiones.g2r(angulo));
+	PuntoVector3D t = n.clonar();
+	n = new PuntoVector3D(
+		cs * t.getX() - sn * v.getX(),
+		cs * t.getY() - sn * v.getY(),
+		cs * t.getZ() - sn * v.getZ());
+	v = new PuntoVector3D(
+		sn * t.getX() + cs * v.getX(),
+		sn * t.getY() + cs * v.getY(),
+		sn * t.getZ() + cs * v.getZ());
+	setModelViewMatrix();
     }
-    
-    public void pitch(double angulo){
-        double cs = Math.cos(Conversiones.g2r(angulo));
-        double sn = Math.sin(Conversiones.g2r(angulo));
-        PuntoVector3D t = n.clonar();
-        n = new PuntoVector3D(
-                cs*t.getX()-sn*v.getX(),
-                cs*t.getY()-sn*v.getY(),
-                cs*t.getZ()-sn*v.getZ());
-        v = new PuntoVector3D(
-                sn*t.getX()+cs*v.getX(),
-                sn*t.getY()+cs*v.getY(),
-                sn*t.getZ()+cs*v.getZ());
-        setModelViewMatrix();
+
+    public void yaw(double angulo) {
+	double cs = Math.cos(Conversiones.g2r(angulo));
+	double sn = Math.sin(Conversiones.g2r(angulo));
+	PuntoVector3D t = u.clonar();
+	u = new PuntoVector3D(
+		cs * t.getX() - sn * v.getX(),
+		cs * t.getY() - sn * v.getY(),
+		cs * t.getZ() - sn * v.getZ());
+	n = new PuntoVector3D(
+		sn * t.getX() + cs * v.getX(),
+		sn * t.getY() + cs * v.getY(),
+		sn * t.getZ() + cs * v.getZ());
+	setModelViewMatrix();
     }
-    
-    public void yaw(double angulo){
-        double cs = Math.cos(Conversiones.g2r(angulo));
-        double sn = Math.sin(Conversiones.g2r(angulo));
-        PuntoVector3D t = u.clonar();
-        u = new PuntoVector3D(
-                cs*t.getX()-sn*v.getX(),
-                cs*t.getY()-sn*v.getY(),
-                cs*t.getZ()-sn*v.getZ());
-        n = new PuntoVector3D(
-                sn*t.getX()+cs*v.getX(),
-                sn*t.getY()+cs*v.getY(),
-                sn*t.getZ()+cs*v.getZ());
-        setModelViewMatrix();
+
+    public void desliza(PuntoVector3D des) {
+	eye.x += des.x;
+	eye.y += des.y;
+	eye.z += des.z;
+
+	setModelViewMatrix();
     }
-    
-    public void desliza(PuntoVector3D des){
-        eye.x+= des.x+des.y+des.z;
-        eye.y+= des.x+des.y+des.z;
-        eye.z+= des.x+des.y+des.z;
-        
-        setModelViewMatrix();
-    }
-    
+
     // Getters & Setters
     public PuntoVector3D getEye() {
-        return eye;
+	return eye;
     }
-    
+
     public void setEye(PuntoVector3D ojo) {
-        this.eye = ojo;
+	this.eye = ojo;
     }
 
     public PuntoVector3D getLook() {
-        return look;
+	return look;
     }
-    
+
     public void setLook(PuntoVector3D vista) {
-        this.look = vista;
+	this.look = vista;
     }
-    
+
     public PuntoVector3D getUp() {
-        return eye;
+	return eye;
     }
-    
+
     public void setUp(PuntoVector3D normal) {
-        this.up = normal;
+	this.up = normal;
     }
-    
 }
