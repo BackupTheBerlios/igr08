91d53856be12d66d51448001cee975e3505d6e50
ivan_rome <ivan_rome>
2008-04-20 15:11:58 +0000

    no message

--- P5/src/practica5/Modelo/Basic/Camara.java
+++ P5/src/practica5/Modelo/Basic/Camara.java
@@ -2,6 +2,7 @@ package practica5.Modelo.Basic;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.glu.GLU;
+import practica5.Controlador.GL3D;
 import practica5.util.Conversiones;
 
 public class Camara {
@@ -28,7 +29,7 @@ public class Camara {
 	 */
 	setView(eye, look, up);
 	setModelViewMatrix();
-	setProjection();
+	setProjection(GL3D.PROY_ORTOGONAL);
     }
 
     private void setView(PuntoVector3D eye, PuntoVector3D look, PuntoVector3D up) {
@@ -45,46 +46,32 @@ public class Camara {
 
 	gl.glMatrixMode(gl.GL_MODELVIEW);
 	gl.glLoadIdentity();
-
 	glu.gluLookAt(eye.getX(), eye.getY(), eye.getZ(), look.getX(), look.getY(), look.getZ(), up.getX(), up.getY(), up.getZ());
     }
 
-    private void setProjection() {
+    private void setProjection(int tipoProyeccion) {
 	gl.glMatrixMode(gl.GL_PROJECTION);
 	gl.glLoadIdentity();
-	//gl.glOrtho(l,r,b,t,-1,1);
-	double anguloVision = 10;
-	double proporcion = 1;
-	glu.gluPerspective(anguloVision, proporcion, N, F);
+
+	switch (tipoProyeccion) {
+	    case GL3D.PROY_ORTOGONAL:
+		gl.glOrtho(l, r, b, t, -1, 1);
+		break;
+	    case GL3D.PROY_PERSPECTIVA:
+		double anguloVision = 10;
+		double proporcion = 1;
+		glu.gluPerspective(anguloVision, proporcion, N, F);
+		break;
+	    case GL3D.PROY_OBLICUA:
+		// Completar
+		break;
+	    default:
+		gl.glOrtho(l, r, b, t, -1, 1);
+		break;
+	}
     }
 
     public void setModelViewMatrix() {
-	/*double x, y, z;
-	x = -eye.prodEsc(u);
-	y = -eye.prodEsc(v);
-	z = -eye.prodEsc(n);
-	PuntoVector3D d = new PuntoVector3D(x, y, z);
-	1 Fila
-	m[0] = u.x;
-	m[1] = u.y;
-	m[2] = u.z;
-	m[3] = d.x;
-	// 2 Fila
-	m[4] = v.x;
-	m[5] = v.y;
-	m[6] = v.z;
-	m[7] = d.y;
-	// 3 Fila
-	m[8] = n.x;
-	m[9] = n.y;
-	m[10] = n.z;
-	m[11] = d.z;
-	// 4 Fila
-	m[12] = 0;
-	m[13] = 0;
-	m[14] = 0;
-	m[15] = 1;*/
-
 	m[0] = u.x;
 	m[1] = v.x;
 	m[2] = n.x;
@@ -93,9 +80,9 @@ public class Camara {
 	m[5] = v.y;
 	m[6] = n.y;
 	m[7] = 0;
-	m[8] = u.y;
-	m[9] = v.y;
-	m[10] = n.y;
+	m[8] = u.z;
+	m[9] = v.z;
+	m[10] = n.z;
 	m[11] = 0;
 	m[12] = -eye.prodEsc(u);
 	m[13] = -eye.prodEsc(v);
@@ -141,13 +128,14 @@ public class Camara {
 	double sn = Math.sin(Conversiones.g2r(angulo));
 	PuntoVector3D t = u.clonar();
 	u = new PuntoVector3D(
-		cs * t.getX() - sn * v.getX(),
-		cs * t.getY() - sn * v.getY(),
-		cs * t.getZ() - sn * v.getZ());
+		cs * t.getX() - sn * n.getX(),
+		cs * t.getY() - sn * n.getY(),
+		cs * t.getZ() - sn * n.getZ());
 	n = new PuntoVector3D(
-		sn * t.getX() + cs * v.getX(),
-		sn * t.getY() + cs * v.getY(),
-		sn * t.getZ() + cs * v.getZ());
+		sn * t.getX() + cs * n.getX(),
+		sn * t.getY() + cs * n.getY(),
+		sn * t.getZ() + cs * n.getZ());
+
 	setModelViewMatrix();
     }
 
